19a7eb029cf617ea72cd2e36a320c04dbe83b765
cimsuxhuki01 <amararifi1234@gmail.com>
2026-03-26 17:40:40 +0100

    a

--- lesson 5/file_operations.py
+++ lesson 5/file_operations.py
@@ -11,3 +11,40 @@ import os
 # open and closing files
 file = open('example.txt','r')
 file.close()
+
+# using with statement for automatic closing
+with open('example.txt','r') as file:
+    content = file.read()
+
+# reading from files
+with open('example.txt','r') as file:
+    content = file.read() #read entire content
+    line = file.readline() # read only 1 line
+    lines = file.readlines() # read all lines into a list
+
+# writing to files
+with open('example.txt','w') as file:
+    file.write('hi')
+
+lines = ['Hello World!\n', 'Welcome to Python\n']
+with open('example.txt','w') as file:
+    file.writelines(lines)
+
+# moving the cursor
+with open('example.txt','r') as file:
+    file.seek(0)
+    data = file.read()
+    print(data)
+
+# checking file existence
+if os.path.exists('example.txt'):
+    print('File Found!')
+
+# appending to file
+with open('example.txt','a') as file:
+    file.write('New data appended')
+
+# reading and writing binary files
+data = b'This is some binary data'
+with open('example.bin','wb') as file:
+    file.write(data)
